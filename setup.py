cbdeff9ee5d82e62f43d28626320933eb5816810
Ashhar-24 <mdashhar01@gmail.com>
2024-05-18 10:59:00 +0530

    Initial setup completed.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Medical Chatbot',
+    version='0.0.0',
+    author='Md Ashhar',
+    author_email='mdashhar01@gmail.com',
+    packages=find_packages(),
+    install_requires=[]
+
+)
---
+++ test_installation.py
@@ -0,0 +1,10 @@
+# test_installation.py
+import transformers
+import flask
+import sentence_transformers
+import pinecone
+import langchain
+import pypdf
+from dotenv import load_dotenv
+
+print("All packages are installed correctly!")
