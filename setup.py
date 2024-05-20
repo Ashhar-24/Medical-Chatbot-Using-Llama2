b66b5e19af9a0aea28e7cb16d272b4cb18a28ad1
Ashhar-24 <mdashhar01@gmail.com>
2024-05-20 11:32:56 +0530

    setup file added

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version='0.0.0',
     author='Md Ashhar',
     author_email='mdashhar01@gmail.com',
-    packages=find_packages(),
+    packages=find_packages(),               # will search in every folder for __init.py__ file and make it a local package
     install_requires=[]
 
 )
